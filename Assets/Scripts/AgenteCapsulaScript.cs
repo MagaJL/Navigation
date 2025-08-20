f4d285d99388c52580cb3e4e951e9563aed34db5
MagaJL <48716048@est.ort.edu.ar>
2025-08-20 09:08:23 -0300

    Intento con navigation y agente

---
+++ Assets/Scripts/AgenteCapsulaScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgenteCapsulaScript : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    public Transform targetTR;
+    [SerializeField] Animator anim;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        agent.destination = targetTR.position;
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+    }
+}
